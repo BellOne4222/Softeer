ecd90b8643cb1a540f0203bc59d34f6010f6189d
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-23 18:18:13 +0900

    05/23

--- practice.py
+++ practice.py
@@ -1,4 +1,7 @@
-ascending_set = set(range(1, 9))  # 1부터 8까지의 숫자 집합
-print(ascending_set)
-descending_set = {sorted(set(range(8, 0, -1)), reverse= True)}
-print(descending_set)
+dot = 0
+
+for i in range(1,3):
+    ans = i*2
+    dot += ans
+
+print(dot)
