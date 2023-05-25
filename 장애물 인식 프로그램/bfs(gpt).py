b935eeb5f2deccc5722671456294f3a7ae5cb714
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-25 17:56:30 +0900

    05/25

--- 장애물 인식 프로그램/bfs(gpt).py
+++ 장애물 인식 프로그램/bfs(gpt).py
@@ -9,63 +9,29 @@ def find_blocks(grid):
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우 방향
 
     def bfs(row, col):
-        queue = deque([(row, col)])  # 큐를 사용하여코드 예시를 계속해서 제시하겠습니다. 아래에는 세 번째 구현 방식을 소개합니다.
+        queue = deque([(row, col)])  # 큐를 사용하여 BFS 구현
+        count = 0  # 블록 내 장애물 개수를 카운트하기 위한 변수
 
-# **구현 방식 3: Union-Find 알고리즘**
-class UnionFind:
-    def __init__(self, size):
-        self.parent = [i for i in range(size)]
-        self.size = [1] * size
+        while queue:
+            r, c = queue.popleft()  # 큐에서 현재 위치 꺼내기
 
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+            if not visited[r][c] and grid[r][c] == 1:
+                visited[r][c] = True  # 현재 위치를 방문으로 처리
+                count += 1  # 장애물 개수 카운트
 
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    # 인접한 위치가 범위 내에 있고 장애물이 있다면 큐에 추가
+                    if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc] and grid[nr][nc] == 1:
+                        queue.append((nr, nc))
 
-        if root_x != root_y:
-            if self.size[root_x] < self.size[root_y]:
-                self.parent[root_x] = root_y
-                self.size[root_y] += self.size[root_x]
-            else:
-                self.parent[root_y] = root_x
-                self.size[root_x] += self.size[root_y]
+        return count
 
-
-def find_blocks(grid):
-    n = len(grid)  # 지도의 크기 N
-    size = n * n  # 노드의 개수
-
-    union_find = UnionFind(size)  # Union-Find 자료구조 생성
-
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우 방향
-
-    # Union-Find를 활용하여 블록 별로 연결된 노드들을 합침
     for i in range(n):
         for j in range(n):
-            if grid[i][j] == 1:
-                curr_node = i * n + j  # 현재 노드의 인덱스
-
-                # 인접한 노드와 연결
-                for dx, dy in directions:
-                    nx, ny = i + dx, j + dy
-                    if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 1:
-                        next_node = nx * n + ny  # 인접한 노드의 인덱스
-                        union_find.union(curr_node, next_node)
-
-    # 각 블록 내 장애물 개수 구하기
-    block_sizes = [0] * size
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == 1:
-                node = i * n + j  # 현재 노드의 인덱스
-                root = union_find.find(node)  # 현재 노드의 루트 노드
-                block_sizes[root] += 1
-
-    blocks = [size for size in block_sizes if size > 0]
+            if not visited[i][j] and grid[i][j] == 1:
+                block_size = bfs(i, j)  # 새로운 블록을 찾아서 BFS 탐색 시작
+                blocks.append(block_size)
 
     return blocks
 
--- 장애물 인식 프로그램/내 풀이.py
+++ 장애물 인식 프로그램/내 풀이.py
@@ -1,49 +1,45 @@
-def find_blocks(grid):
-    n = len(grid)  # 지도의 크기 N
+def find_obstacles(grid):
+    n = len(grid)
 
-    visited = [[False] * n for _ in range(n)]  # 방문 여부를 나타내는 2차원 배열
-    blocks = []  # 블록들을 저장할 리스트
+    visited = [[False] * n for _ in range(n)]
+    
+    blocks = [] # 블록 저장할 리스트
 
     def dfs(row, col):
-        if row < 0 or row >= n or col < 0 or col >= n:
-            return 0  # 인덱스가 범위를 벗어나면 0을 반환
-        if visited[row][col] or grid[row][col] == 0:
-            return 0  # 이미 방문했거나 도로가 있는 경우 0을 반환
+        if row < 0 or row >= n or col < 0 or col >= n: # 지도 범위가 넘어가면 
+            return 0
+        if visited[row][col] or grid[row][col] == 0: # 이미 방문을 했거나 도로가 있으면면
+            return 0
+        
+        visited[row][col] = True # 방문 처리
+        block_count = 1 # 블록의 장애물 개수
 
-        visited[row][col] = True  # 현재 위치를 방문으로 처리
-        count = 1  # 현재 장애물을 포함한 블록의 장애물 개수
+        block_count += dfs(row - 1, col) # 상
+        block_count += dfs(row + 1, col) # 하
+        block_count += dfs(row, col - 1) # 좌
+        block_count += dfs(row, col + 1) # 우
 
-        # 상하좌우로 재귀적으로 탐색
-        count += dfs(row - 1, col)  # 상
-        count += dfs(row + 1, col)  # 하
-        count += dfs(row, col - 1)  # 좌
-        count += dfs(row, col + 1)  # 우
-
-        return count
+        return block_count
 
     for i in range(n):
         for j in range(n):
-            if not visited[i][j] and grid[i][j] == 1:
-                block_size = dfs(i, j)  # 새로운 블록을 찾아서 탐색 시작
-                blocks.append(block_size)
-
+            if not visited[i][j] and grid[i][j] == 1: # 방문을 안했고, 장애물이 있으면
+                block = dfs(i, j) # 새로운 블록 탐색
+                blocks.append(block) # 블록 내 장애물 찾으면면 추가
     return blocks
 
 
-# 입력 받기
-n = int(input())  # 지도의 크기 N
-grid = []
+n = int(input()) # 지도 크기
+grid = [] # 지도
 for _ in range(n):
-    row = list(map(int, input().strip())) # input은 한 줄을 입력받는 함수고, strip은 양 끝 공백을 없애주는 함수입니다. 그리고 split은 특정 단위로 나눠주는 함수입니다.
-    grid.append(row)
+    rows = list(map(int, input().strip()))
+    grid.append(rows)
 
-# 장애물 블록 찾기
-result = find_blocks(grid)
+answer = find_obstacles(grid)
 
-# 블록 수와 블록 내 장애물 수 출력
-print(len(result))  # 총 블록 수 출력
-for count in sorted(result):  # 오름차순으로 정렬하여 출력
-    print(count)
+print(len(answer)) # 총 블록수
+for ans in sorted(answer):
+    print(ans) # 오름차순 정렬해서 블록 수 
 
 
 # 이 코드는 깊이 우선 탐색(DFS)을 활용하여 장애물 블록을 찾습니다. 각 블록을 찾을 때마다 해당 블록에 속하는 장애물의 수를 구하여 리스트에 저장합니다. 마지막으로 블록 수와 블록 내 장애물 수를 출력합니다.
